85c8668d150e50e85cd0a2fd03d9d88f90989f07
Jordan Daniels <decentpianist@gmail.com>
2018-02-15 16:19:55 -0800

    nav bar collapses appropriately when screen size is smaller ADD COMMENTS ON PICTURES

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -10,22 +10,28 @@ class NavBar extends Component {
 	render(){
 		return(
 			<div className="container" style={{letterSpacing: "2px"}}>
-			<nav className="navbar navbar-fixed-top navbar-toggleable-md navbar-dark bg-faded" style={{marginTop: "3em", zIndex: 2}} >
-				<div className="container-fluid">
-					<div className="navbar-header">
-					  <a className="navbar-brand"><img src={Logo} alt="D3C Kings" style={{height: "3em", width: "15em"}}/></a>
-					</div>
-		      <a className="nav-link" href="#" >ARCHITECTURE</a>
-	        <a className="nav-link" href="#" >RESIDENCE</a>
-	        <a className="nav-link" href="#" >FLOOR PLANS</a>
-	        <a className="nav-link" href="#" >AMENITIES</a>
-	        <a className="nav-link" href="#" >NEIGHBORHOOD</a>
-	        <a className="nav-link" href="#" >OUR TEAM</a>
-	        <a className="nav-link" href="#" >PRESS </a>
-	        <a className="nav-link btn btn-outline-secondary" href="#">INQUIRE</a>
+			<nav className="navbar navbar-expand-lg" style={{marginTop: "3em", zIndex: 2}} role="navigation" >
+				
+				<button className="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+	          &#9776;
+	      </button>
+	      <div className="navbar-header">
+				  <a className="navbar-brand"><img src={Logo} alt="D3C Kings" style={{height: "3em", width: "15em"}}/></a>
+				</div>
+	      <div className="collapse navbar-collapse" id="exCollapsingNavbar">
+	        <ul className="nav navbar-nav">
+	            <li className="nav-item"><a className="nav-link" style={{paddingRight: "2em"}} href="#" >ARCHITECTURE</a></li>
+	            <li className="nav-item"><a className="nav-link" style={{paddingRight: "2em"}} href="#" >RESIDENCE</a></li>
+	            <li className="nav-item"><a className="nav-link" style={{paddingRight: "2em"}} href="#" >FLOOR PLANS</a></li>
+	            <li className="nav-item"><a className="nav-link" style={{paddingRight: "2em"}} href="#" >AMENITIES</a></li>
+	            <li className="nav-item"><a className="nav-link" style={{paddingRight: "2em"}} href="#" >NEIGHBORHOOD</a></li>
+	            <li className="nav-item"><a className="nav-link" style={{paddingRight: "2em"}} href="#" >OUR TEAM</a></li>
+	            <li className="nav-item"><a className="nav-link" style={{paddingRight: "2em"}} href="#" >PRESS </a></li>
+	            <li className="nav-item"><a className="nav-link btn btn-outline-secondary" href="#">INQUIRE</a></li>
+	        </ul>
 				</div>
 			</nav>
-			</div>
+				</div>
 		)
 	}
 }

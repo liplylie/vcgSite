11f24e4cd7cb0553f83905ba31a9b1a411c4ffd0
Jordan Daniels <decentpianist@gmail.com>
2018-02-15 17:12:49 -0800

    architecture page is made FINISH THE REST OF THE PAGE MESSAGES

--- client/components/Home.js
+++ client/components/Home.js
@@ -4,8 +4,11 @@ import { ScrollPage, Section } from 'react-scrollpage';
 import Hollywood from '../static/Hollywood_copy.jpg'
 import KingsRoadCam from '../static/KingsRoadCam.jpg'
 import KingsRoadUnit from '../static/KingsRoadUnit.jpg'
+import KingsRoadUnit2 from '../static/KingUnit2.jpg'
 import KingsRoadHome1 from '../static/KingsRoadHome1.jpg'
 import KingsRoadHome2 from '../static/KingsRoadHome2.jpg'
+import KingsTreadmill from '../static/KingTreadmill.jpeg'
+import KingsShower from '../static/KingShower.jpg'
 
 class Home extends Component {
 	constructor(){
@@ -14,20 +17,22 @@ class Home extends Component {
 
 	render(){
 		const imageStyle = {height: "100vh", width: "100vw"}
-			const options = {
-				curPage: 1,        
-				totalPage: 5,         
-				delay: 1200           
-			}
+		const options = {
+			curPage: 1,        
+			totalPage: 7,         
+			delay: 1000           
+		}
 
 		return(
 			<div id="HomeDiv">
 			<ScrollPage {...options}>
-				<Section><img src={KingsRoadHome1} style={imageStyle}/></Section>
-				<Section><img src={KingsRoadHome2} style={imageStyle}/></Section>
-				<Section><img src={Hollywood} style={imageStyle}/></Section>
-				<Section><img src={KingsRoadUnit} style={imageStyle}/></Section>
-				<Section><img src={KingsRoadCam} style={imageStyle}/></Section>
+				<Section><img id="topPicture" src={KingsRoadHome1} style={imageStyle}/></Section>
+				<Section><img id="Architecture" src={KingsRoadHome2} style={imageStyle}/><div className="architecture-message"><p>Architecture</p><button type="button" className="btn btn-outline-secondary btn-lg">Learn More</button></div></Section>
+				<Section><img id="Residence"src={KingsRoadUnit} style={imageStyle}/></Section>
+				<Section><img id="Amenities" src={KingsTreadmill} style={imageStyle}/></Section>
+				<Section><img id="Neighborhood" src={Hollywood} style={imageStyle}/></Section>
+				<Section><img id="Press" src={KingsRoadUnit2} style={imageStyle}/></Section>
+				<Section><img id="Inquire" src={KingsShower} style={imageStyle}/></Section>
 				</ScrollPage>
 			</div>
 		)
